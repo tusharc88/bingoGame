b62a79124a05184fddf06132675fa39751f32c1b
tusharc88 <tushar54983@gmail.com>
2018-05-08 08:36:05 -0700

    Making the tests work for Cell

--- client/src/components/Cell/Cell.test.js
+++ client/src/components/Cell/Cell.test.js
@@ -7,7 +7,7 @@ import Cell from './index';
 const wrap = (props = {}) =>
   shallow(
     <Cell currentNumber={1} {...props}>
-      1
+      {1}
     </Cell>
   );
 
@@ -31,7 +31,7 @@ test('handleDisabled should return true when currentNumber is 2 and Cell number 
   const newWrap = (props = {}) =>
     shallow(
       <Cell currentNumber={2} {...props}>
-        1
+        {1}
       </Cell>
     );
 
@@ -40,8 +40,22 @@ test('handleDisabled should return true when currentNumber is 2 and Cell number 
 });
 
 test('handleClick should toggle background color on clicking non disabled cell', () => {
-  const cell = wrapper.find('div').at(0);
-  cell.simulate('click');
-  // console.log(`cell ${cell}`);
-  // expect(wrapper.instance().handleDisabled()).toEqual(false);
+  wrapper.simulate('click');
+  expect(wrapper).toHaveStyleRule('background-color', '#3ecedf');
+  wrapper.simulate('click');
+  expect(wrapper).toHaveStyleRule('background-color', '#d7d3d2');
+});
+
+test('handleClick should not toggle background color on clicking disabled cell', () => {
+  const newWrap = (props = {}) =>
+    shallow(
+      <Cell currentNumber={2} {...props}>
+        {1}
+      </Cell>
+    );
+
+  const newWrapper = newWrap();
+
+  newWrapper.simulate('click');
+  expect(newWrapper).toHaveStyleRule('background-color', '#d7d3d2');
 });
